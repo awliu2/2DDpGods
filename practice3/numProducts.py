61b5b35957fe57969dfba66a1455c0720a4c0b53
Je Yeong - Pycharm <jeyeong@uchicago.edu>
2022-10-29 15:03:22 -0500

    problem F

--- practice3/numProducts.py
+++ practice3/numProducts.py
@@ -2,34 +2,35 @@ from sys import stdin
 
 lines = [l.strip() for l in stdin.readlines()]
 
-len_n, nums = int(lines[0]), [int(s) for s in lines[1].split()]
+n, nums = int(lines[0]), [int(s) for s in lines[1].split()]
 
-# print(len_n, nums)
+dp = [[0, 0, 0] for _ in range(n)]
 
-dp = []
+if nums[0] > 0:
+    dp[0][0] += 1
+elif nums[0] == 0:
+    dp[0][1] += 1
+else:
+    dp[0][2] += 1
 
-dp.append(nums[0])
-for i in range(1, len_n):
-    if dp[i - 1] == 0:
-        dp.append(nums[i])
-    else: 
-        dp.append(dp[i - 1] * nums[i])
-print(dp)
-
-neg = 0
-zero = 0
-pos = 0
-
-for l in range(len_n):
-    for r in range(l, len_n):
-        if l == 0 or dp[l - 1] == 0:
-            prod = dp[r]
+for i, num in enumerate(nums):
+    if i > 0:
+        if num > 0:
+            dp[i][0] += 1
+            dp[i][0] += dp[i - 1][0]
+            dp[i][1] += dp[i - 1][1]
+            dp[i][2] += dp[i - 1][2]
+        elif num == 0:
+            dp[i][1] += 1
+            dp[i][1] += dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]
         else:
-            prod = dp[r] / dp[l - 1]
-        if prod < 0: neg += 1
-        if prod == 0: zero += 1
-        if prod > 0: pos += 1
+            dp[i][2] += 1
+            dp[i][0] += dp[i - 1][2]
+            dp[i][1] += dp[i - 1][1]
+            dp[i][2] += dp[i - 1][0]
 
-print(neg, zero, pos)
+numPositive = sum([x for x, _, _ in dp])
+numZero = sum([x for _, x, _ in dp])
+numNegative = sum([x for _, _, x in dp])
 
-
+print(numNegative, numZero, numPositive)
